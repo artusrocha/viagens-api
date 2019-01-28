32f38fa9f48387a6a8bcf9c13bc6b400e159cbaf
Alexandre Artus Rocha Bezerra <artusrocha@gmail.com>
2019-01-28 10:18:04 -0200

    Commit inicial - apenas spring-boot boilerplate

---
+++ src/main/java/demo/viagensapi/ViagensaapiApplication.java
@@ -0,0 +1,14 @@
+package demo.viagensapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViagensaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViagensaapiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/demo/viagensaapi/ViagensaapiApplicationTests.java
@@ -0,0 +1,17 @@
+package demo.viagensaapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViagensaapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
