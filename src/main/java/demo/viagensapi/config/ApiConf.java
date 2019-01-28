bcc0cc4c9f9b03528514a3a82128a45ed8053936
Alexandre Artus Rocha Bezerra <artusrocha@gmail.com>
2019-01-28 10:38:23 -0200

    adicionando configurações

---
+++ src/main/java/demo/viagensapi/config/ApiConf.java
@@ -0,0 +1,9 @@
+package demo.viagensapi.config;
+
+public class ApiConf {
+
+	public static final double FEE_CALC_BASE = 1.3;
+	public static final String HOTEL_ENDPOINT = "https://cvcbackendhotel.herokuapp.com/hotels/";
+	public static final String CITY_ENDPOINT = "https://cvcbackendhotel.herokuapp.com/hotels/avail/";
+	
+}
