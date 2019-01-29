d6dcc2e5d2323a3e99bf32a21c2a32b1218e5e72
Alexandre Artus Rocha Bezerra <artusrocha@gmail.com>
2019-01-29 12:03:45 -0200

    ajuste no calculo, adicionar fee para os preços indiveiduais diarios

--- src/main/java/demo/viagensapi/helper/Calc.java
+++ src/main/java/demo/viagensapi/helper/Calc.java
@@ -12,31 +12,35 @@ import demo.viagensapi.dto.output.RoomDtoOut;
 @Component
 public class Calc {
 
-	public RoomDtoOut price( RoomDtoOut roomDto,  ReqArgs args){
+	public RoomDtoOut price(RoomDtoOut roomDto, ReqArgs args) {
 		long days = ChronoUnit.DAYS.between(args.getCheckin(), args.getCheckout());
 		double priceAdults = args.getAdults() * roomDto.getPriceDetail().getPricePerDayAdult() * days;
 		double priceChilds = args.getChilds() * roomDto.getPriceDetail().getPricePerDayChild() * days;
 		roomDto.setTotalPrice(priceAdults + priceChilds);
 		return roomDto;
-	} 
-	
-	public HotelDtoOut price( HotelDtoOut dto,  ReqArgs args){
-		for( RoomDtoOut roomDto : dto.getRooms() ) {
+	}
+
+	public HotelDtoOut price(HotelDtoOut dto, ReqArgs args) {
+		for (RoomDtoOut roomDto : dto.getRooms()) {
 			price(roomDto, args);
 		}
 		return dto;
 	}
-	
-	public RoomDtoOut addFee( RoomDtoOut roomDto,  ReqArgs args ) {
+
+	public RoomDtoOut addFee(RoomDtoOut roomDto, ReqArgs args) {
 		roomDto.setTotalPrice(roomDto.getTotalPrice() * ApiConf.FEE_CALC_BASE);
+		roomDto.getPriceDetail()
+				.setPricePerDayAdult(roomDto.getPriceDetail().getPricePerDayAdult() * ApiConf.FEE_CALC_BASE);
+		roomDto.getPriceDetail()
+				.setPricePerDayChild( roomDto.getPriceDetail().getPricePerDayChild() * ApiConf.FEE_CALC_BASE);
 		return roomDto;
-	} 
-	
-	public HotelDtoOut addFee( HotelDtoOut dto,  ReqArgs args){
-		for( RoomDtoOut roomDto : dto.getRooms() ) {
+	}
+
+	public HotelDtoOut addFee(HotelDtoOut dto, ReqArgs args) {
+		for (RoomDtoOut roomDto : dto.getRooms()) {
 			addFee(roomDto, args);
 		}
 		return dto;
 	}
-	
+
 }
